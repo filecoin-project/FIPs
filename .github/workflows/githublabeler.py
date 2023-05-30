cd532e3012d686bb6b62b62a40919020d8a6d4f1
NateWebb03 <nate@fil.org>
2023-05-30 16:58:38 -0700

    Updated with gql code vs all python library

--- .github/workflows/githublabeler.py
+++ .github/workflows/githublabeler.py
@@ -1,44 +1,175 @@
+#Suggestions from Ian - Change timedelta from 30 days to a few minutes
 from datetime import datetime, timedelta
-from github import Github
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 import os
+import json
 
-# Authentication token for your GitHub account
-try:
-    GITHUB_TOKEN = os.getenv("TOKEN_SECRET")
-except KeyError:
-    GITHUB_TOKEN = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+#Select github's api transport
+transport = AIOHTTPTransport(
+    url='https://api.github.com/graphql',
+    headers={'Authorization': f'Bearer {GITHUB_TOKEN}'}
+)
 
-# The repository to check discussion posts for
-REPO_NAME = "FIPs"
+#getAllDiscussions instantiates a client connected to github's api transport and sends a query for all discussion objects in a given date range
+#startDateTime: datetime object for the start of the range
+#endDateTime: datetime object for the end of the range
+#return: a list of ids
 
-# Labels for discussion posts
-NEW_LABEL = "New"
-ACTIVE_LABEL = "Active"
-QUIET_LABEL = "Quiet"
 
-# Time delta for new discussions (30 days) and active discussions (2 months)
-NEW_DELTA = timedelta(days=30)
-ACTIVE_DELTA = timedelta(days=60)
 
-# Initialize the PyGithub object with the authentication token
-g = Github(GITHUB_TOKEN)
-
-# Get the repository object for the specified repository name
-repo = g.get_repo(REPO_NAME)
-
-# Get all the discussions for the repository
-discussions = repo.get_discussions()
-
-# Iterate through the discussions and label them accordingly
-for discussion in discussions:
-    last_update = discussion.updated_at.replace(tzinfo=None)
-    time_since_last_update = datetime.utcnow() - last_update
-    if time_since_last_update <= NEW_DELTA:
-        discussion.add_to_labels(NEW_LABEL) 
-    elif time_since_last_update <= ACTIVE_DELTA:
-        discussion.add_to_labels(ACTIVE_LABEL)     
+def getAllDiscussions():
+  ids = []
+  discussionPosts = []
+  client = Client(transport=transport, fetch_schema_from_transport=True)
+  endCursor = ""
+  while True:
+    params = {'endCursor': endCursor }
+    query = gql(
+        """
+    query getAllDiscussions($endCursor: String!){
+      repository(owner: "NateWebb03", name: "FIPs") {
+        discussions(first: 100, after: $endCursor, orderBy: {field: UPDATED_AT, direction: DESC}) {
+          pageInfo{
+          endCursor
+          hasNextPage
+          }
+          # type: DiscussionConnection
+          totalCount # Int!
+          
+          nodes {
+            #type: Discussion
+            id
+            lastEditedAt
+            createdAt
+            comments(first: 1){
+              nodes{
+              #Type: Comment
+                id
+                createdAt
+                updatedAt
+                publishedAt
+                replies(first: 1){
+                  nodes{
+                    id
+                    createdAt
+                    lastEditedAt
+                    updatedAt
+                    publishedAt
+                  }
+                }
+              }
+            }
+            labels(first: 10){
+            #Type: LabelConnection
+              nodes{
+              #Type: Label
+                name
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+        """
+    ) if endCursor != "" else gql(
+        """
+    query getAllDiscussions{
+      repository(owner: "NateWebb03", name: "FIPs") {
+        discussions(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}) {
+          pageInfo{
+          endCursor
+          hasNextPage
+          }
+          # type: DiscussionConnection
+          totalCount # Int!
+          
+          nodes {
+            #type: Discussion
+            id
+            lastEditedAt
+            createdAt
+            comments(first: 1){
+              nodes{
+              #Type: Comment
+                id
+                createdAt
+                updatedAt
+                publishedAt
+                replies(first: 1){
+                  nodes{
+                    id
+                    createdAt
+                    lastEditedAt
+                    updatedAt
+                    publishedAt
+                  }
+                }
+              }
+            }
+            labels(first: 10){
+            #Type: LabelConnection
+              nodes{
+              #Type: Label
+                name
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+        """
+    )
+    result = client.execute(query, variable_values=params)
+    discussionPosts += (result['repository']['discussions']['nodes'])
+    if result['repository']['discussions']['pageInfo']['hasNextPage']:
+      endCursor = result['repository']['discussions']['pageInfo']['endCursor']
+      continue
     else:
-        discussion.add_to_labels(QUIET_LABEL)
-        
+      break
+
+  print(len(discussionPosts))
+  return discussionPosts
+
+#TODO: Add logic checking comments: updatedAt  replies: updatedAt & 
+def isActive(discussionPost):
+  return True
+
+def updateDiscussions(discussionPosts, currentDateTime):
+  client = Client(transport=transport, fetch_schema_from_transport=True)
+  QUIET_LABEL = 'LA_kwDOCq44tc7jNGmM'
+  ACTIVE_LABEL = 'LA_kwDOCq44tc7jNGan'
+  NEW_LABEL = 'LA_kwDOCq44tc7jNGRJ'
+  updates = []
+  for d in discussionPosts:
+    labelsToAdd = []
+    labelsToRemove = []
+    if datetime.datetime.fromisoformat(d['createdAt']) < currentDateTime - timedelta(days = 30):
+      labelsToRemove += NEW_LABEL
+    else:
+      labelsToAdd += NEW_LABEL
+    if isActive():
+      labelsToAdd += ACTIVE_LABEL
+      labelsToRemove += QUIET_LABEL
+    else:
+      labelsToRemove += ACTIVE_LABEL
+      labelsToAdd += QUIET_LABEL
+    updates += {'id': d['id'], 'add': labelsToAdd, 'remove': labelsToRemove}
+    
+    
+  for u in updates: 
+    mutate = gql(
+      """
+      mutation addLabel($id: ID!, $add: [ID!]!, $remove: [ID!]!){
+        addLabelsToLabelable(input: {labelIds: $add, labelableId: $id})
+        removeLabelsFromLabelable(input: {labelIds: $remove, labelableId: $id})
+      }
+      """
+    )
+  result = client.execute(mutate, variable_values=u)
+  print(result)
+  return
+print(getAllDiscussions())
