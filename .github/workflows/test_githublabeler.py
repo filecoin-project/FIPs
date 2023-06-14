9c833454ceca36c1a95f071085294b916a72ae8f
Ian Davis <jungziege@gmail.com>
2023-06-14 10:51:50 -0700

    Add additional test cases for githublabeler

--- .github/workflows/test_githublabeler.py
+++ .github/workflows/test_githublabeler.py
@@ -4,12 +4,26 @@ from datetime import datetime, timezone
 
 FAKE_ID = 'fakeID'
 
-activeDiscussionPosts = [{'id': FAKE_ID,
+activeDiscussionPosts = [{'id': FAKE_ID, # active because of lastEditedAt only
                           'createdAt': '2022-06-01T22:42:26Z',
                           'lastEditedAt': '2023-06-01T22:42:26Z',
-                          'comments': {'nodes': [{'createdAt': '2023-06-01T22:42:26Z',
+                          'comments': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
+                                                  'updatedAt': '2022-06-01T22:42:26Z',
+                                                  'replies': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
+                                                                         'updatedAt': '2022-06-01T22:42:26Z'}]}}]}},
+                         {'id': FAKE_ID, # active because of comments only
+                          'createdAt': '2022-06-01T22:42:26Z',
+                          'lastEditedAt': '2022-06-01T22:42:26Z',
+                          'comments': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
                                                   'updatedAt': '2023-06-01T22:42:26Z',
-                                                  'replies': {'nodes': [{'createdAt': '2023-06-01T22:42:26Z',
+                                                  'replies': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
+                                                                         'updatedAt': '2022-06-01T22:42:26Z'}]}}]}},
+                         {'id': FAKE_ID, # active because of comment reply only
+                          'createdAt': '2022-06-01T22:42:26Z',
+                          'lastEditedAt': '2022-06-01T22:42:26Z',
+                          'comments': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
+                                                  'updatedAt': '2022-06-01T22:42:26Z',
+                                                  'replies': {'nodes': [{'createdAt': '2022-06-01T22:42:26Z',
                                                                          'updatedAt': '2023-06-01T22:42:26Z'}]}}]}}]
 
 quietDiscussionPosts = [{'id': FAKE_ID,
@@ -44,14 +58,27 @@ def test_getUpdates():
         'id': FAKE_ID,
         'add': [
             githublabeler.QUIET_LABEL], 'remove': [
-            githublabeler.NEW_LABEL, githublabeler.ACTIVE_LABEL]}]
+            githublabeler.NEW_LABEL, githublabeler.ACTIVE_LABEL]
+    }]
     assert githublabeler.getUpdates(activeDiscussionPosts, now) == [{
         'id': FAKE_ID,
         'add': [
             githublabeler.ACTIVE_LABEL], 'remove': [
-            githublabeler.NEW_LABEL, githublabeler.QUIET_LABEL]}]
+            githublabeler.NEW_LABEL, githublabeler.QUIET_LABEL]
+    }, {
+        'id': FAKE_ID,
+        'add': [
+            githublabeler.ACTIVE_LABEL], 'remove': [
+            githublabeler.NEW_LABEL, githublabeler.QUIET_LABEL]
+    }, {
+        'id': FAKE_ID,
+        'add': [
+            githublabeler.ACTIVE_LABEL], 'remove': [
+            githublabeler.NEW_LABEL, githublabeler.QUIET_LABEL]
+    }]
     assert githublabeler.getUpdates(newDiscussionPosts, now) == [{
         'id': FAKE_ID,
         'add': [
             githublabeler.NEW_LABEL, githublabeler.ACTIVE_LABEL], 'remove': [
-            githublabeler.QUIET_LABEL]}]
+            githublabeler.QUIET_LABEL]
+    }]
